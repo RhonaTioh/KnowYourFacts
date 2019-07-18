dc424b79b13116e9dc91992892be0f197048ad5e
17010318 <17010318@myrp.edu.sg>
2019-07-18 11:18:01 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/knowyourfacts/Frag2.java
@@ -0,0 +1,63 @@
+package sg.edu.rp.c346.knowyourfacts;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.squareup.picasso.Picasso;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Frag2 extends Fragment {
+
+    Button btnColor;
+    LinearLayout linear2;
+
+    ImageView iv;
+    Integer i = 0;
+
+    public Frag2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_frag2, container,false);
+
+        btnColor = view.findViewById(R.id.btnColour2);
+        linear2 = view.findViewById(R.id.linear2);
+
+        iv = view.findViewById(R.id.iv2);
+
+        btnColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (i == 0) {
+                    linear2.setBackgroundColor(Color.RED);
+                    i = 1;
+                } else {
+                    linear2.setBackgroundColor(Color.GRAY);
+                    i = 0;
+                }
+            }
+        });
+
+        String imageUrl = "https://wtffunfact.com/wp-content/uploads/2019/06/fun-fact-holographic-circus.png";
+        Picasso.with(getActivity()).load(imageUrl).into(iv);
+
+        return view;
+    }
+
+}
